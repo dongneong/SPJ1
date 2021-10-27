93a9f2475d0f6ae006434f735d94e9a5df9a423f
dongneong <dongneong2@naver.com>
2021-10-27 21:22:42 +0900

    seperate main.js and topic.js,index.js for routing

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
+
 var bodyParser = require('body-parser');
 var compression = require('compression');
-
+var topicRouter = require('./routes/topic');
+var indexRouter = require('./routes/index');
 
 // 웹서버로 요청이 들어올때마다 타게되는 미들웨어
 app.use(express.static('public')); // 정적파일 사용하기 위해 추가하는 미들웨어
@@ -21,152 +18,8 @@ app.use(function(request, response, next){
   })
 });
 
-app.get('/', (request, response) => {
-  var title = 'Welcome';
-  var description = 'Hello, Node.js';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list,
-    `<h2>${title}</h2>${description}
-    <img src="/images/korea.jpg" style="width:300px; display:block; margin-top:10px;"/>
-    `
-    ,
-    `<a href="/topic/create">create</a>`
-  );
-  response.send(html);
-});
-
-app.get('/topic/page/:pageId', (request, response) => {
-  console.log(request.list);
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-          <a href="/topic/update/${sanitizedTitle}">update</a>
-          <form action="/topic/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    });
-});
-
-app.get('/topic/create', (request, response)=>{
-    var title = 'WEB - create';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list, `
-      <form action="/topic/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-});
-
-app.post('/topic/create_process', (request, response)=> {
-  var post = request.body; // body-parser를 활용하여 body데이터 갖고오기...완전간편
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-    response.redirect(`/topic/page/${title}`)
-  });
-  // var body = '';
-  // request.on('data', function(data){
-  //     body = body + data;
-  // });
-  // request.on('end', function(){
-  //     var post = qs.parse(body);
-  //     var title = post.title;
-  //     var description = post.description;
-  //     fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-  //       response.redirect(`/page/${title}`)
-  //     })
-  // });
-});
-
-app.get('/topic/update/:pageId', (request, response)=> {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var list = template.list(request.list);
-      var html = template.HTML(title, list,
-        `
-        <form action="/topic/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/topic/create">create</a> <a href="/update?id=${title}">update</a>`
-      );
-      response.send(html);
-    });
-});
-
-app.post('/topic/update_process', (request, response) => {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function(error){
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/topic/page/${title}`)
-    })
-  });
-  // var body = '';
-  // request.on('data', function(data){
-  //     body = body + data;
-  // });
-  // request.on('end', function(){
-  //     var post = qs.parse(body);
-  //     var id = post.id;
-  //     var title = post.title;
-  //     var description = post.description;
-  //     fs.rename(`data/${id}`, `data/${title}`, function(error){
-  //       fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-  //         response.redirect(`/page/${title}`)
-  //       })
-  //     });
-  // });
-});
-
-app.post('/topic/delete_process', (request, response) => {
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function(error){
-    response.redirect('/');
-  });
-  // var body = '';
-  // request.on('data', function(data){
-  //     body = body + data;
-  // });
-  // request.on('end', function(){
-  //     var post = qs.parse(body);
-  //     var id = post.id;
-  //     var filteredId = path.parse(id).base;
-  //     fs.unlink(`data/${filteredId}`, function(error){
-  //       response.redirect('/');
-  //     })
-  // });
-});
+app.use('/', indexRouter);
+app.use('/topic', topicRouter); 
 
 // app.use(function(reqeust, response, next) {
 //   response.status(404).send("Sorry, 404 NOT FOUND...");
